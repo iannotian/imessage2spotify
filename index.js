88d70bdb4b21307f16e876d2f3b2aad4713c4f7a
Ian M <ian@localhost.localdomain>
2020-12-09 17:23:37 -0500

    first

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const axios = require('axios');
+const qs = require('qs');
+
+require('dotenv').config();
+
+const app = express();
+const server = require('http').Server(app);
+
+const redirect = 'https://imessage2spotify.herokuapp.com/callback';
+
+app.get('/add', async (req, res) => {
+
+});
+
+app.get('/authorize', async (req, res) => {
+    const spotifyURL = new URL('https://accounts.spotify.com/authorize');
+    spotifyURL.searchParams.append('client_id', process.env.CLIENT_ID);
+    spotifyURL.searchParams.append('response_type', 'code');
+    spotifyURL.searchParams.append('redirect_uri', redirect);
+
+    res.redirect(spotifyURL.toString());
+});
+
+app.get('/callback', async (req, res) => {
+    const { code, error } = req.params;
+
+    if (error !== undefined) {
+        res.status(403).end();
+    }
+    else if (code !== undefined) {
+        const data = await axios({
+            method: 'post',
+            url: 'https://accounts.spotify.com/api/token',
+            data: qs.stringify({
+                grant_type: 'authorization_code',
+                code: code,
+                redirect_uri: redirect
+            }),
+            headers: {
+                "Authorization": `Basic ${Buffer.from(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`).toString('base64')}`,
+                "content-type": 'application/x-www-form-urlencoded'
+            }
+        });
+
+        res.json(data);
+    }
+});
+
+port = process.env.PORT || 8080;
+
+server.listen(port, async () => {
+    console.log(`server listening on port ${port}`);
+});
