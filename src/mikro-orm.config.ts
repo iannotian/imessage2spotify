83953fb8b96b66d2a7cdcbe86d83eeb91f339777
Ian MacFarlane <1900909+iannotian@users.noreply.github.com>
2021-11-13 00:28:49 -0500

    add db url for heroku

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -5,12 +5,18 @@ import { Song } from "./entities/songs.entity";
 export default {
   metadataProvider: TsMorphMetadataProvider,
   entities: [Song],
-  user: "user",
-  password: "password",
-  dbName: "imessage2spotify",
+  ...(process.env.NODE_ENV === "production" && {
+    clientUrl: process.env.DATABASE_URL,
+  }),
+  ...(process.env.NODE_ENV !== "production" && {
+    user: "user",
+    password: "password",
+    dbName: "imessage2spotify",
+  }),
   type: "postgresql",
   migrations: {
     path: process.cwd() + "/src/migrations",
+    disableForeignKeys: false,
   },
   ...(process.env.NODE_ENV === "production" && {
     driverOptions: {
