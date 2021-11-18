6c8cccea390c757e68a7a7cd3a603aa232cea734
Ian MacFarlane <1900909+iannotian@users.noreply.github.com>
2021-11-17 21:14:14 -0500

    refactor formatTimeAgo

--- src/util.ts
+++ src/util.ts
@@ -1,22 +1,31 @@
 export function formatTimeAgo(date: Date, locale?: string): string {
-  const formatter = new Intl.RelativeTimeFormat(locale);
-  const deltaDays = (date.getTime() - Date.now()) / (1000 * 3600 * 24);
+  const unitToMsMap: { [key: string]: number } = {
+    year: 1000 * 3600 * 24 * 365,
+    month: 1000 * 3600 * 24 * 30,
+    week: 1000 * 3600 * 24 * 7,
+    day: 1000 * 3600 * 24,
+    hour: 1000 * 3600,
+    minute: 1000 * 60,
+    second: 1000,
+  };
 
-  if (Math.abs(deltaDays) < 1) {
-    const deltaHours = (date.getTime() - Date.now()) / (1000 * 3600);
-    if (Math.abs(deltaHours) < 1) {
-      const deltaMinutes = (date.getTime() - Date.now()) / (1000 * 60);
+  let remainingMs = date.getTime() - Date.now();
+  let timeDelta = 0;
+  let lastUnit: any = "year";
 
-      if (Math.abs(deltaMinutes) < 1) {
-        const deltaSeconds = (date.getTime() - Date.now()) / 1000;
-        return formatter.format(Math.round(deltaSeconds), "seconds");
-      } else {
-        return formatter.format(Math.round(deltaMinutes), "minutes");
-      }
+  for (const [unit, unitMs] of Object.entries(unitToMsMap)) {
+    if (remainingMs > unitMs) {
+      remainingMs = remainingMs % unitMs;
     } else {
-      return formatter.format(Math.round(deltaHours), "hours");
+      timeDelta = (date.getTime() - Date.now()) / unitMs;
+      lastUnit = unit;
+
+      if (Math.abs(timeDelta) >= 1) {
+        break;
+      }
     }
-  } else {
-    return formatter.format(Math.round(deltaDays), "days");
   }
+
+  const formatter = new Intl.RelativeTimeFormat(locale);
+  return formatter.format(Math.round(timeDelta), lastUnit);
 }
